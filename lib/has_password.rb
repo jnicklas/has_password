a616a89ecc46bae23cbe8aaae32ba615534af757
Jonas Nicklas and Anders Tornqvist <dev+jnicklas+unders@elabs.se>
2009-07-23 09:04:33 +0200

    Moved macro into base file

--- lib/has_password.rb
+++ lib/has_password.rb
@@ -1,25 +1,59 @@
-require File.dirname(__FILE__) + '/has_password/macro'
-
 require 'digest/sha1'
 
 module HasPassword
   
   FORBIDDEN = %w(password user system test admin)
   
-  # Returns a random string of the given length
-  def self.random_hex(length = 8)
-    (1..length).map { |i| rand(16).to_s(16) }.join('')
+  class << self
+    # Returns a random string of the given length
+    def random_hex(length = 8)
+      (1..length).map { |i| rand(16).to_s(16) }.join('')
+    end
+  
+    # Returns the SHA1 hash of the string with the given salt
+    def encrypt(string, salt = '')
+      hashable = "#{salt}#{string}"
+      Digest::SHA1.hexdigest(hashable)
+    end
+  
+    def included(base)
+      super
+      base.send :extend, HasPassword
+    end
   end
   
-  # Returns the SHA1 hash of the string with the given salt
-  def self.encrypt(string, salt = '')
-    hashable = "#{salt}#{string}"
-    Digest::SHA1.hexdigest(hashable)
+  def has_password(options = {})
+    include HasPassword::InstanceMethods
+    
+    # Store salt size in chars rather than bits. One hex char == 4 bits
+    @salt_length = ((options[:salt_size] || 24) / 4.0).ceil
+    
+    validates_format_of :password_hash, :with => /^[0-9a-f]{40}$/
+    validates_format_of :password_salt, :with => %r{^[0-9a-f]{#{@salt_length}}$}
+    validates_confirmation_of :password
+    
+    define_callbacks :after_password_change
+
+    after_update do |m|
+      m.instance_eval do
+        callback(:after_password_change) if password_hash_changed?
+      end
+    end
+    
+    validate do |m|
+      unless m.password.blank?
+        HasPassword::FORBIDDEN.each do |word|
+          m.errors.add(:password, "may not contain the string '#{word}'") if
+              m.password =~ Regexp.new(word, Regexp::IGNORECASE)
+        end
+      end
+      
+      m.errors.add(:password, 'must not be blank') if m.new_record? and m.password.blank?
+    end
   end
   
-  def self.included(base)
-    base.send :include, InstanceMethods
-    base.send :extend, ClassMethods
+  def salt_length
+    @salt_length
   end
   
   module InstanceMethods
@@ -43,10 +77,6 @@ module HasPassword
     end
   end
   
-  module ClassMethods
-    def salt_length
-      @salt_length
-    end
-  end
-  
 end
+
+ActiveRecord::Base.send(:include, HasPassword) if defined?(ActiveRecord)
